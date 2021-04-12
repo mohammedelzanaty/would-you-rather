6464471a4d7067e3becc29b6ff20fb1463e426ed
mohammdelzanaty <mohamed.elzanaty3@vodafone.com>
2021-04-12 08:27:58 +0200

    add dashboard questions

--- src/components/Question.js
+++ src/components/Question.js
@@ -8,6 +8,7 @@ import {
   makeStyles,
   Typography,
 } from '@material-ui/core'
+import PropTypes from 'prop-types' // ES6
 import { Link } from 'react-router-dom'
 import { Divider } from './index'
 
@@ -17,49 +18,28 @@ const useStyles = makeStyles((theme) => ({
   },
 }))
 
-const question = {
-  id: '8xf0y6ziyjabvozdd253nd',
-  author: 'sarahedo',
-  timestamp: 1467166872634,
-  optionOne: {
-    votes: ['sarahedo'],
-    text: 'have horrible short term memory',
-  },
-  optionTwo: {
-    votes: [],
-    text: 'have horrible long term memory',
-  },
-}
-
-const Question = () => {
+const Question = ({ id, avatarURL, author, optionOne, optionTwo }) => {
   const classes = useStyles()
   return (
     <Card variant="outlined">
       <CardContent>
         <Grid container>
           <Grid item md={2}>
-            <img
-              src={`https://randomuser.me/api/portraits/men/32.jpg`}
-              alt={`question avatar`}
-            />
+            <img src={avatarURL} alt={`${author} avatar`} />
           </Grid>
           <Grid item md={10}>
             <Typography gutterBottom>
-              <span className={classes.title}>Mohammed Elzanaty</span> asks
-              Would you rather
+              <span className={classes.title}>{author}</span> asks Would you
+              rather
             </Typography>
-            <Typography color="textSecondary">
-              {question.optionOne.text}
-            </Typography>
+            <Typography color="textSecondary">{optionOne.text}</Typography>
             <Divider>Or</Divider>
-            <Typography color="textSecondary">
-              {question.optionTwo.text}?
-            </Typography>
+            <Typography color="textSecondary">{optionTwo.text}?</Typography>
           </Grid>
         </Grid>
       </CardContent>
       <CardActions>
-        <Link to={`/question/${1}`} style={{ width: '100%' }}>
+        <Link to={`/question/${id}`} style={{ width: '100%' }}>
           <Button type="submit" fullWidth variant="contained" color="secondary">
             View Question
           </Button>
@@ -67,6 +47,17 @@ const Question = () => {
       </CardActions>
     </Card>
   )
+}
+
+Question.protoTypes = {
+  id: PropTypes.string.isRequired,
+  avatarURL: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  optionOne: PropTypes.shape({
+    votes: PropTypes.array.isRequired,
+    text: PropTypes.string.isRequired,
+  }).isRequired,
+  optionTwo: PropTypes.array.isRequired,
 }
 
 export default Question
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Container, Tab, Tabs } from '@material-ui/core'
+import { connect } from 'react-redux'
+import sortBy from 'sort-by'
 import { Question, TabPanel } from '../components'
 
 function a11yProps(index) {
@@ -18,24 +20,68 @@ class Dashboard extends Component {
     this.setState({ value })
   }
 
+  formatQuestions = (questionsIds) =>
+    questionsIds
+      .map((questionId) => ({
+        author: this.props.questions[questionId].author,
+        id: questionId,
+        optionOne: this.props.questions[questionId].optionOne,
+        optionTwo: this.props.questions[questionId].optionTwo,
+        timestamp: this.props.questions[questionId].timestamp,
+        avatarURL: this.props.users[this.props.authedUser].avatarURL,
+      }))
+      .sort(sortBy('-timestamp'))
+
   render() {
     const { value } = this.state
+    const { questions, authedUser } = this.props
+    const answeredQuestions = Object.keys(questions).filter(
+      (questionId) =>
+        questions[questionId].optionOne.votes.includes(authedUser) ||
+        questions[questionId].optionTwo.votes.includes(authedUser)
+    )
+
+    const unAnsweredQuestions = Object.keys(questions).filter(
+      (questionId) =>
+        !questions[questionId].optionOne.votes.includes(authedUser) ||
+        !questions[questionId].optionTwo.votes.includes(authedUser)
+    )
+
+    console.log({ unAnsweredQuestions, answeredQuestions })
     return (
       <Container className="content">
         <h1 className="content__title">Dashboard</h1>
         <Tabs value={value} onChange={this.handleChange}>
-          <Tab label={`Unanswered Questions ${1}`} {...a11yProps(0)} />
-          <Tab label={`Answered Questions ${4}`} {...a11yProps(1)} />
+          <Tab
+            label={`Unanswered Questions ${unAnsweredQuestions.length}`}
+            {...a11yProps(0)}
+          />
+          <Tab
+            label={`Answered Questions ${answeredQuestions.length}`}
+            {...a11yProps(1)}
+          />
         </Tabs>
         <TabPanel value={value} index={0}>
-          <Question />
+          {unAnsweredQuestions &&
+            this.formatQuestions(unAnsweredQuestions).map((question) => (
+              <Question {...question} key={question.id} />
+            ))}
         </TabPanel>
         <TabPanel value={value} index={1}>
-          <Question />
+          {answeredQuestions &&
+            this.formatQuestions(answeredQuestions).map((question) => (
+              <Question {...question} key={question.id} />
+            ))}
         </TabPanel>
       </Container>
     )
   }
 }
 
-export default Dashboard
+const mapStateToProps = ({ users, questions, authedUser }) => ({
+  questions,
+  users,
+  authedUser,
+})
+
+export default connect(mapStateToProps)(Dashboard)
